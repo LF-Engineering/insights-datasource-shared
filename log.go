d5b13cf3990391c947341959d9086c1ad710aca4
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-08-11 15:11:35 +0000

    Fmt
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- log.go
+++ log.go
@@ -8,7 +8,7 @@ import (
 
 // Printf is a wrapper around Printf(...) that supports logging and removes redacted data.
 func Printf(format string, args ...interface{}) {
-  // TODO: FIXME: wrapper log function that also pushes everything to log elasticsearch cluster (cc @Fayaz)
+	// TODO: FIXME: wrapper log function that also pushes everything to log elasticsearch cluster (cc @Fayaz)
 	// Actual logging to stdout & DB
 	now := time.Now()
 	msg := FilterRedacted(fmt.Sprintf("%s: "+format, append([]interface{}{ToYMDHMSDate(now)}, args...)...))
