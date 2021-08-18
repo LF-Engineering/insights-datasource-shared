821f08c7a5982d5a8a3e8c346bdda68b6e2fd99f
Ayman <enkhalifapro@gmail.com>
2021-08-18 15:15:49 +0200

    finish putRecord and putRecords funcs

--- firehose/firehose.go
+++ firehose/firehose.go
@@ -1,6 +1,8 @@
 package firehose
 
 import (
+	"encoding/json"
+	"errors"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/firehose"
@@ -12,9 +14,35 @@ const (
 	region        = "AWS_REGION"
 	defaultRegion = "us-east-1"
 )
+
 type Config struct {
 	Endpoint string
 	Region   string
+}
+
+// ClientProvider for kinesis firehose
+type ClientProvider struct {
+	firehose *firehose.Firehose
+	region   string
+	endPoint string
+}
+
+// NewClientProvider initiate new client provider
+func NewClientProvider() *ClientProvider {
+	c := &ClientProvider{}
+	c.region = os.Getenv(region)
+	if c.region == "" {
+		log.Printf("No AWS Region found for env var AWS_REGION. setting defaultRegion=%s \n", defaultRegion)
+		c.region = defaultRegion
+	}
+
+	if os.Getenv("LOCALSTACK_HOSTNAME") != "" {
+		c.endPoint = os.Getenv("LOCALSTACK_HOSTNAME")
+	}
+	sess := session.Must(session.NewSession())
+	c.firehose = firehose.New(sess, aws.NewConfig().WithRegion(c.region).WithEndpoint(c.endPoint))
+
+	return c
 }
 
 func LoadConfig() *Config {
@@ -27,26 +55,59 @@ func LoadConfig() *Config {
 		Region: r,
 	}
 
-		if os.Getenv("LOCALSTACK_HOSTNAME") != "" {
-			cfg.Endpoint = os.Getenv("LOCALSTACK_HOSTNAME")
-		}
+	if os.Getenv("LOCALSTACK_HOSTNAME") != "" {
+		cfg.Endpoint = os.Getenv("LOCALSTACK_HOSTNAME")
+	}
 	return &cfg
 }
 
-func PutRecordBatch(c Config, channel string, data []byte) (int64,error) {
+func (c *ClientProvider) PutRecordBatch(channel string, records []interface{}) (map[string]error, error) {
+	inputs := make([]*firehose.Record, 0)
+	for _, r := range records {
+		b, err := json.Marshal(r)
+		if err != nil {
+			return map[string]error{}, err
+		}
+		records = append(records, &firehose.Record{Data: b})
+	}
 
-	sess := session.Must(session.NewSession())
-	svr := firehose.New(sess, aws.NewConfig().WithRegion(c.Region).WithEndpoint(c.Endpoint))
-
-	var records []*firehose.Record
-	records = append(records, &firehose.Record{Data: data})
 	params := &firehose.PutRecordBatchInput{
 		DeliveryStreamName: aws.String(channel),
-		Records:            records,
+		Records:            inputs,
 	}
-	recordBatch, err := svr.PutRecordBatch(params)
+	recordBatch, err := c.firehose.PutRecordBatch(params)
 	if err != nil {
-		return 0, err
+		return map[string]error{}, err
 	}
-	return *recordBatch.FailedPutCount, nil
+
+	results := make(map[string]error)
+	for _, res := range recordBatch.RequestResponses {
+		if res.RecordId != nil {
+			if res.ErrorMessage != nil && *res.ErrorMessage != "" {
+				results[*res.RecordId] = errors.New(*res.ErrorMessage)
+			} else {
+				results[*res.RecordId] = nil
+			}
+		}
+
+	}
+	return results, nil
+}
+
+// PutRecord ...
+func (c *ClientProvider) PutRecord(channel string, record interface{}) (string, error) {
+	b, err := json.Marshal(record)
+	if err != nil {
+		return "", err
+	}
+	params := &firehose.PutRecordInput{
+		DeliveryStreamName: aws.String(channel),
+		Record:            &firehose.Record{Data: b},
+	}
+	res, err := c.firehose.PutRecord(params)
+	if err != nil {
+		return "", err
+	}
+
+	return *res.RecordId, nil
 }
