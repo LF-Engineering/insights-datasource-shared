9b4a362cc5bed537120be057662fd21a4084d4fc
Ayman <enkhalifapro@gmail.com>
2021-08-18 16:12:02 +0200

    code clean up

--- firehose/firehose.go
+++ firehose/firehose.go
@@ -3,12 +3,14 @@ package firehose
 import (
 	"encoding/json"
 	"errors"
+	"log"
+	"os"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/firehose"
-	"log"
-	"os"
 )
+
 
 const (
 	region        = "AWS_REGION"
