716194606cc644e2d1b34d02dc9bcdcb0e60d4d9
Ayman <enkhalifapro@gmail.com>
2022-01-05 13:18:37 +0200

    add elastic gap
    Signed-off-by: Ayman <enkhalifapro@gmail.com>

---
+++ elasticgap/dto.go
@@ -0,0 +1,19 @@
+package elasticgap
+
+// ElasticResponse ...
+type ElasticResponse struct {
+	Took   int
+	Errors bool
+	Items  []ElasticResponseItem
+}
+
+// ElasticResponseItem ...
+type ElasticResponseItem struct {
+	Index ESResponseIndex
+}
+
+// ESResponseIndex ...
+type ESResponseIndex struct {
+	ID     string `json:"_id"`
+	Status int
+}
