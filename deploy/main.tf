d7544316a657b7c65b31311e944632ab4fbceda3
Ajinkya Nahar <ajinkyan@proximabiz.com>
2022-01-13 22:19:59 +0530

    DA-4648: add jira connector ecs task terraform configuration
    Signed-off-by: Ajinkya Nahar <ajinkyan@proximabiz.com>

--- deploy/main.tf
+++ deploy/main.tf
@@ -44,6 +44,37 @@ resource "aws_ecs_task_definition" "insights-git-task" {
 
 }
 
+/* ECS task definitions */
+resource "aws_ecs_task_definition" "insights-connector-jira-task" {
+  family = "insights-connector-jira-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode = "awsvpc"
+  cpu = "256"
+  memory = "512"
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn = aws_iam_role.ecs_task_role.arn
+  container_definitions = jsonencode([
+    {
+      name      = "insights-connector-jira"
+      image     = "395594542180.dkr.ecr.us-east-1.amazonaws.com/insights-connector-jira:latest"
+      cpu       = 128
+      memory    = 512
+      essential = true
+      logConfiguration: {
+        "logDriver": "awslogs",
+        "options": {
+          "awslogs-group": "insights-connector-jira-logs",
+          "awslogs-region": "us-east-2",
+          "awslogs-create-group": "true",
+          "awslogs-stream-prefix": "ecs"
+        }
+      }
+    }
+  ])
+
+}
+
+
 resource "aws_security_group" "security_group" {
   name        = "example-task-security-group"
   vpc_id      = aws_vpc.main.id
