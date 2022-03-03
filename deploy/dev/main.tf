4ae80fb7d015b6300a3e9e51bb98d16935068e8e
Ayman <enkhalifapro@gmail.com>
2022-03-04 01:02:56 +0200

    update bucket encryption
    Signed-off-by: Ayman <enkhalifapro@gmail.com>

--- deploy/dev/main.tf
+++ deploy/dev/main.tf
@@ -39,6 +39,17 @@ resource "aws_s3_bucket_acl" "terraform-state-acl" {
   acl    = "private"
 }
 
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform-state-encryption-configuration" {
+  bucket = aws_s3_bucket.terraform-state.id
+
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.terraform-bucket-key.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+}
+
 resource "aws_s3_bucket_public_access_block" "block" {
   bucket = aws_s3_bucket.terraform-state.id
 
@@ -762,27 +773,4 @@ resource "aws_iam_role_policy_attachment" "task_role_cloudwatch_policy_attachmen
 resource "aws_iam_role_policy_attachment" "task_execution_role_cloudwatch_policy_attachment" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
-}
-
-data "aws_iam_policy_document" "kms_use" {
-  statement {
-    sid = ""
-    effect = "Allow"
-    actions = [
-      "kms:Encrypt",
-      "kms:Decrypt",
-      "kms:ReEncrypt*",
-      "kms:GenerateDataKey*",
-      "kms:DescribeKey",
-    ]
-    resources = [
-      "arn:aws:kms:us-east-2:${var.eg_account_id}:key/f36a45d3-9bce-4f10-bedc-5a20c2ff807e"
-    ]
-  }
-}
-
-resource "aws_iam_policy" "kms_use" {
-  name        = "kmsuse"
-  description = "Policy to allow use of KMS Key"
-  policy      = data.aws_iam_policy_document.kms_use.json
 }
