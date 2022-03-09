86d4781f419a6bf68d04bf0b0c0695dbe646a046
Łukasz Gryglicki <lgryglicki@cncf.io>
2022-03-09 06:36:02 +0000

    Test this
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- log.go
+++ log.go
@@ -35,7 +35,6 @@ func SetSyncMode(sync bool) {
 
 // Printf is a wrapper around Printf(...) that supports logging and removes redacted data.
 func Printf(format string, args ...interface{}) {
-	// TODO: FIXME: wrapper log function that also pushes everything to log elasticsearch cluster (cc @Fayaz)
 	// Actual logging to stdout & DB
 	now := time.Now()
 	msg := FilterRedacted(fmt.Sprintf("%s: "+format, append([]interface{}{ToYMDHMSDate(now)}, args...)...))
