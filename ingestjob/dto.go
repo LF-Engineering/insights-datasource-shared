757379614a31f45a529dd43867c593ec9c2698b6
Ibrahim <dev.code.ibra@gmail.com>
2022-03-10 20:46:47 +0300

    add function to write ecs tasks
    Signed-off-by: Ibrahim <dev.code.ibra@gmail.com>

--- ingestjob/dto.go
+++ ingestjob/dto.go
@@ -29,3 +29,27 @@ type NestedHits struct {
 	ID     string `json:"_id"`
 	Source Log    `json:"_source"`
 }
+
+// TaskTopHits result
+type TaskTopHits struct {
+	Hits TaskHits `json:"hits"`
+}
+
+// TaskHits result
+type TaskHits struct {
+	Hits []TaskNestedHits `json:"hits"`
+}
+
+// TaskNestedHits is the actual hit data
+type TaskNestedHits struct {
+	ID     string  `json:"_id"`
+	Source TaskLog `json:"_source"`
+}
+
+type TaskLog struct {
+	Id            string              `json:"id"`            // task id
+	Connector     string              `json:"connector"`     // name of connector as https://github.com/LF-Engineering/lfx-event-schema/blob/main/service/insights/shared.go#L32-L49
+	EndpointId    string              `json:"endpoint_id"`   // generated ID of the endpoint per lfx-event-schema generator functions
+	Configuration []map[string]string `json:"configuration"` // metadata
+	CreatedAt     *time.Time          `json:"created_at,omitempty"`
+}
