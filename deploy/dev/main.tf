e612703fc1ac58b9ca6cde72a86f37fb11dc3a2c
Fayaz <5818912+fayazg@users.noreply.github.com>
2022-04-01 09:32:50 -0600

    pipermail connector running out of memory (#52)

--- deploy/dev/main.tf
+++ deploy/dev/main.tf
@@ -407,16 +407,16 @@ resource "aws_ecs_task_definition" "insights-connector-pipermail-task" {
   family = "insights-connector-pipermail-task"
   requires_compatibilities = ["FARGATE"]
   network_mode = "awsvpc"
-  cpu = "256"
-  memory = "512"
+  cpu = "516"
+  memory = "2048"
   execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
   task_role_arn = aws_iam_role.ecs_task_role.arn
   container_definitions = jsonencode([
     {
       name      = "insights-connector-pipermail"
       image     = "${var.eg_account_id}.dkr.ecr.${var.eg_aws_region}.amazonaws.com/insights-connector-pipermail:latest"
-      cpu       = 128
-      memory    = 512
+      cpu       = 516
+      memory    = 2048
       essential = true
       logConfiguration: {
         "logDriver": "awslogs",
