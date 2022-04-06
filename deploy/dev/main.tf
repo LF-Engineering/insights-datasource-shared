3f2a1a9aa08a6208b83bf49914e3e048e9f0bcd9
Ayman <enkhalifapro@gmail.com>
2022-04-06 14:41:00 +0200

    clean up
    Signed-off-by: Ayman <enkhalifapro@gmail.com>

--- deploy/dev/main.tf
+++ deploy/dev/main.tf
@@ -12,9 +12,6 @@ terraform {
     encrypt        = true
     kms_key_id     = "alias/terraform-bucket-key"
   }
-  required_providers {
-    aws  = "~> 3.74.1"
-  }
 }
 
 resource "aws_kms_key" "terraform-bucket-key" {
