b79dd5881a7b740510e551caa77d4eed3bee506b
Ayman <enkhalifapro@gmail.com>
2022-06-12 18:07:41 +0200

    clean up
    Signed-off-by: Ayman <enkhalifapro@gmail.com>

--- deploy/dev/main.tf
+++ deploy/dev/main.tf
@@ -708,3 +708,48 @@ resource "aws_ecs_service" "insights-scheduler" {
     assign_public_ip = true
   }
 }
+
+/* iam roles */
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "insights-ecs-task-execution-role"
+
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": [
+          "ecs-tasks.amazonaws.com",
+          "cloudwatch.amazonaws.com"
+        ]
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+
+resource "aws_iam_role" "ecs_task_role" {
+  name = "insights-ecs-task-role"
+
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "ecs-tasks.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
