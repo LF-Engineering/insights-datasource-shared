2876b8c6099b0f95d5abd78c2a1fd64f6bc25984
Ayman <enkhalifapro@gmail.com>
2022-06-12 17:31:33 +0200

    clean up
    Signed-off-by: Ayman <enkhalifapro@gmail.com>

--- deploy/dev/main.tf
+++ deploy/dev/main.tf
@@ -661,3 +661,12 @@ resource "aws_security_group" "security_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Main"
+  }
+}
