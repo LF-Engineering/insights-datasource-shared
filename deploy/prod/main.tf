7aafd9b3b7b0f710d007bcb51ce9fdfd810e83a2
khalifapro <81648906+khalifapro@users.noreply.github.com>
2022-06-15 21:09:38 +0200

    increase git connector memory (#64)
    Signed-off-by: Ayman <enkhalifapro@gmail.com>
    
    Co-authored-by: Ayman <enkhalifapro@gmail.com>

--- deploy/prod/main.tf
+++ deploy/prod/main.tf
@@ -115,7 +115,7 @@ resource "aws_ecs_task_definition" "insights-connector-git-task" {
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
   cpu                      = "256"
-  memory                   = "1024"
+  memory                   = "2048"
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   task_role_arn            = aws_iam_role.ecs_task_role.arn
   container_definitions    = jsonencode([
