e950a087c99059d07e136142bbdbc65ec909ee24
Ayman <enkhalifapro@gmail.com>
2022-06-21 20:10:25 +0200

    increase cpu for git connector
    Signed-off-by: Ayman <enkhalifapro@gmail.com>

--- deploy/dev/main.tf
+++ deploy/dev/main.tf
@@ -114,7 +114,7 @@ resource "aws_ecs_task_definition" "insights-connector-git-task" {
   family                   = "insights-connector-git-task"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  cpu                      = "256"
+  cpu                      = "1024"
   memory                   = "2048"
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   task_role_arn            = aws_iam_role.ecs_task_role.arn
@@ -122,8 +122,8 @@ resource "aws_ecs_task_definition" "insights-connector-git-task" {
     {
       name      = "insights-connector-git"
       image     = "${var.eg_account_id}.dkr.ecr.${var.eg_aws_region}.amazonaws.com/insights-connector-git:latest"
-      cpu       = 256
-      memory    = 1024
+      cpu       = 1024
+      memory    = 2048
       essential = true
       logConfiguration : {
         "logDriver" : "awslogs",
