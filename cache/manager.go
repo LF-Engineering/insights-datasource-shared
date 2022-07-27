00f4bfc8e8798d50b3bf9b615aed654770c39fbc
Ayman <enkhalifapro@gmail.com>
2022-07-27 23:45:49 +0200

    add caching get and update file
    Signed-off-by: Ayman <enkhalifapro@gmail.com>

--- cache/manager.go
+++ cache/manager.go
@@ -118,3 +118,36 @@ func (m *Manager) SetLastSync(endpoint string, lastSync time.Time) error {
 	}
 	return nil
 }
+
+// GetFileByKey get file by key
+func (m *Manager) GetFileByKey(endpoint string, id string) ([]byte, error) {
+	key := fmt.Sprintf(Path, m.connector, endpoint, id)
+	data, err := m.s3Manager.Get(key)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case s3.ErrCodeNoSuchKey:
+				return data, nil
+			default:
+				return data, err
+			}
+		}
+		return data, err
+	}
+	return data, nil
+}
+
+// UpdateFileByKey get file by key
+func (m *Manager) UpdateFileByKey(endpoint string, id string, data []byte) ([]byte, error) {
+	key := fmt.Sprintf(Path, m.connector, endpoint, id)
+	b, err := json.Marshal(data)
+	if err != nil {
+		return b, err
+	}
+
+	err = m.s3Manager.SaveWithKey(b, key)
+	if err != nil {
+		return b, err
+	}
+	return data, nil
+}
