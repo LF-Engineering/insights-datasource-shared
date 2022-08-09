db47594a0999b4409b6681278427228e4eb215f0
Ayman <enkhalifapro@gmail.com>
2022-08-09 16:16:04 +0200

    add aws get container arn functionality
    Signed-off-by: Ayman <enkhalifapro@gmail.com>

---
+++ aws/ecs.go
@@ -0,0 +1,33 @@
+package aws
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/LF-Engineering/insights-datasource-shared/http"
+	"os"
+	"time"
+)
+
+func GetContainerARN() (string, error) {
+	httpClient := http.NewClientProvider(60*time.Second, true)
+	statusCode, res, err := httpClient.Request(fmt.Sprintf("%s/task", os.Getenv("ECS_CONTAINER_METADATA_URI_V4")), "GET", nil, nil, nil)
+	if err != nil {
+		return "", err
+	}
+	if statusCode > 200 {
+		return "", fmt.Errorf("getContainerMetadata error: status code is %d", statusCode)
+	}
+
+	metaResponse := ContainerMetadata{}
+	err = json.Unmarshal(res, &metaResponse)
+	if err != nil {
+		return "", err
+	}
+
+	return metaResponse.TaskARN, nil
+}
+
+// ContainerMetadata ...
+type ContainerMetadata struct {
+	TaskARN string `json:"TaskARN"`
+}
