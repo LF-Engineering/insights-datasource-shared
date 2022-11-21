e50247f95afaefad426b885f6f85437bba2a723a
Ayman <enkhalifapro@gmail.com>
2022-11-21 19:12:58 +0200

    instrument git and gerrit
    Signed-off-by: Ayman <enkhalifapro@gmail.com>

--- deploy/prod/main.tf
+++ deploy/prod/main.tf
@@ -168,6 +168,38 @@ resource "aws_ecs_task_definition" "insights-connector-git-task" {
   task_role_arn            = aws_iam_role.ecs_task_role.arn
   container_definitions    = jsonencode([
     {
+      name      = "datadog-agent"
+      image     = "public.ecr.aws/datadog/agent:latest"
+      cpu       = 256
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          protocol      = "tcp",
+          hostPort      = 8126,
+          containerPort = 8126
+        }
+      ]
+      environment : [
+        {
+          "name" : "DD_API_KEY",
+          valueFrom : "arn:aws:ssm:${var.eg_aws_region}:${var.eg_account_id}:parameter/cloudops-datadog-api-key"
+        },
+        {
+          "name" : "ECS_FARGATE",
+          "value" : true
+        },
+        {
+          "name": "DD_SITE",
+          "value": "datadoghq.com"
+        },
+        {
+          "name": "DD_APM_ENABLED"
+          "value": true
+        }
+      ]
+    },
+    {
       name      = "insights-connector-git"
       image     = "${var.eg_account_id}.dkr.ecr.${var.eg_aws_region}.amazonaws.com/insights-connector-git:stable"
       cpu       = 1024
@@ -257,6 +289,38 @@ resource "aws_ecs_task_definition" "insights-connector-gerrit-task" {
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   task_role_arn            = aws_iam_role.ecs_task_role.arn
   container_definitions    = jsonencode([
+    {
+      name      = "datadog-agent"
+      image     = "public.ecr.aws/datadog/agent:latest"
+      cpu       = 256
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          protocol      = "tcp",
+          hostPort      = 8126,
+          containerPort = 8126
+        }
+      ]
+      environment : [
+        {
+          "name" : "DD_API_KEY",
+          valueFrom : "arn:aws:ssm:${var.eg_aws_region}:${var.eg_account_id}:parameter/cloudops-datadog-api-key"
+        },
+        {
+          "name" : "ECS_FARGATE",
+          "value" : true
+        },
+        {
+          "name": "DD_SITE",
+          "value": "datadoghq.com"
+        },
+        {
+          "name": "DD_APM_ENABLED"
+          "value": true
+        }
+      ]
+    },
     {
       name      = "insights-connector-gerrit"
       image     = "${var.eg_account_id}.dkr.ecr.${var.eg_aws_region}.amazonaws.com/insights-connector-gerrit:stable"
