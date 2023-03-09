897558c04689442fd84ec181c3f3279b0a0f39f0
khalifapro <81648906+khalifapro@users.noreply.github.com>
2023-03-09 18:44:36 +0200

    remove datadog container (#112)
    Signed-off-by: Ayman <enkhalifapro@gmail.com>
    Co-authored-by: Ayman <enkhalifapro@gmail.com>

--- deploy/prod/main.tf
+++ deploy/prod/main.tf
@@ -168,48 +168,10 @@ resource "aws_ecs_task_definition" "insights-connector-git-task" {
   task_role_arn            = aws_iam_role.ecs_task_role.arn
   container_definitions    = jsonencode([
     {
-      name      = "datadog-agent"
-      image     = "public.ecr.aws/datadog/agent:latest"
-      cpu       = 256
-      memory    = 512
-      essential = true
-      portMappings = [
-        {
-          protocol      = "tcp",
-          hostPort      = 8126,
-          containerPort = 8126
-        }
-      ]
-      secrets :[
-        {
-          "name" : "DD_API_KEY",
-          valueFrom : "arn:aws:ssm:${var.eg_aws_region}:${var.eg_account_id}:parameter/cloudops-datadog-api-key"
-        },
-      ]
-      environment : [
-        {
-          "name" : "ECS_FARGATE",
-          "value" : "true"
-        },
-        {
-          "name": "DD_SITE",
-          "value": "datadoghq.com"
-        },
-        {
-          "name": "DD_APM_ENABLED"
-          "value": "true"
-        },
-        {
-          "name": "DD_APM_NON_LOCAL_TRAFFIC",
-          "value": "true"
-        }
-      ]
-    },
-    {
       name      = "insights-connector-git"
       image     = "${var.eg_account_id}.dkr.ecr.${var.eg_aws_region}.amazonaws.com/insights-connector-git:stable"
-      cpu       = 1792
-      memory    = 5120
+      cpu       = 2048
+      memory    = 6144
       essential = true
       secrets : [
         {
