945b040fdfc17b2246464cf98394e8238c40f8c8
khalifapro <81648906+khalifapro@users.noreply.github.com>
2023-03-15 15:09:34 +0200

    add http request with request (#92)
    Signed-off-by: Ayman <enkhalifapro@gmail.com>
    Co-authored-by: Ayman <enkhalifapro@gmail.com>

--- http/client.go
+++ http/client.go
@@ -88,6 +88,42 @@ func (h *ClientProvider) RequestWithResponse(url string, method string, header m
 	return res, buf.Bytes(), nil
 }
 
+// RequestWithRequest http
+func (h *ClientProvider) RequestWithRequest(req *http.Request, header map[string]string, params map[string]string) (*http.Response, []byte, error) {
+	if cType, ok := header["Content-Type"]; !ok || cType == "application/json" {
+		req.Header.Add("Content-Type", "application/json")
+		delete(header, "Content-Type")
+	}
+
+	if header != nil {
+		for k, v := range header {
+			req.Header.Add(k, v)
+		}
+	}
+
+	if params != nil {
+		q := req.URL.Query()
+		for k, v := range params {
+			q.Add(k, v)
+		}
+		req.URL.RawQuery = q.Encode()
+	}
+
+	// Do request
+	res, err := h.httpclient.Do(req)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var buf bytes.Buffer
+	_, err = buf.ReadFrom(res.Body)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return res, buf.Bytes(), nil
+}
+
 // RequestCSV requests http API that returns csv result
 func (h *ClientProvider) RequestCSV(url string) ([][]string, error) {
 	resp, err := http.Get(url)
